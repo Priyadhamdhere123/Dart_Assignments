126516e34dc10fda2f3288351096e7d41927631e
priyadhamdhere <root@Priya>
2024-09-24 22:35:45 +0530

    Codes first commit

--- Dart/Assignment/PyramidPatterns1/program6.dart
+++ Dart/Assignment/PyramidPatterns1/program6.dart
@@ -2,20 +2,20 @@ import "dart:io";
 void main(){
 	print("Enter the row:");
 	int row = int.parse(stdin.readLineSync()!);
+	int num=0;
 	for(int i=1;i<=row;i++){
-			int num=0;
 		for(int sp=row;sp>i;sp--){
 			stdout.write("\t");
 		}
 			for(int j=1;j<=i*2-1;j++){
 				if(j<i){
-						num++;
+						
 					stdout.write("$num\t");
-						//num++;
-				}else{
 						num--;
+				}else{
+						
 					stdout.write("$num\t");
-						//num--;
+						num++;
 				}
 			}
 					print(" ");
